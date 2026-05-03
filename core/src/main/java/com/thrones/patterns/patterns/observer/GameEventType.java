157d8a077729cf5badd814fe72bdf4597290dbfa
zarina-kulm <zarina.kulmakhanbet@narxoz.kz>
2026-05-04 00:24:51 +0500

    add GameEventType enum

--- core/src/main/java/com/thrones/patterns/patterns/observer/GameEventType.java
+++ core/src/main/java/com/thrones/patterns/patterns/observer/GameEventType.java
@@ -1,4 +1,13 @@
 package com.thrones.patterns.patterns.observer;
 
-public class GameEventType {
+public enum GameEventType {
+    DAMAGE_TAKEN,
+    CHARACTER_DIED,
+    HEALED,
+    LEVEL_UP,
+    WAVE_STARTED,
+    WAVE_COMPLETED,
+    BOSS_SPAWNED,
+    GOLD_EARNED,
+    ABILITY_USED
 }
