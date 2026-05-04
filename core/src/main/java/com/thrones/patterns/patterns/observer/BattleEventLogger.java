bac0a384642e3c5347912085b610abe5b70cc1a3
zarina-kulm <zarina.kulmakhanbet@narxoz.kz>
2026-05-04 08:30:27 +0500

    add BattleEventLogger implementation

--- core/src/main/java/com/thrones/patterns/patterns/observer/BattleEventLogger.java
+++ core/src/main/java/com/thrones/patterns/patterns/observer/BattleEventLogger.java
@@ -1,4 +1,43 @@
 package com.thrones.patterns.patterns.observer;
 
-public class BattleEventLogger {
+import com.thrones.patterns.characters.Character;
+
+public class BattleEventLogger implements GameEventObserver {
+
+    @Override
+    public void onEvent(GameEventType type, Object data) {
+        String name = (data instanceof Character)
+            ? ((Character) data).getName() : data.toString();
+
+        switch (type) {
+            case DAMAGE_TAKEN:
+                System.out.println("[LOG] " + name + " took damage! HP: " +
+                    (data instanceof Character ? (int)((Character)data).getHp() : "?"));
+                break;
+            case CHARACTER_DIED:
+                System.out.println("[LOG] " + name + " has DIED!");
+                break;
+            case HEALED:
+                System.out.println("[LOG] " + name + " was healed!");
+                break;
+            case LEVEL_UP:
+                System.out.println("[LOG] " + name + " LEVELED UP!");
+                break;
+            case WAVE_STARTED:
+                System.out.println("[LOG] Wave started: " + data);
+                break;
+            case WAVE_COMPLETED:
+                System.out.println("[LOG] Wave completed!");
+                break;
+            case BOSS_SPAWNED:
+                System.out.println("[LOG] !! BOSS: " + name + " !!");
+                break;
+            case GOLD_EARNED:
+                System.out.println("[LOG] Gold earned: " + data);
+                break;
+            case ABILITY_USED:
+                System.out.println("[LOG] Ability used by " + name);
+                break;
+        }
+    }
 }
