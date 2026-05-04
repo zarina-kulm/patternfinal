a9a152b15b0a40406329a68d3dc50de984c9ac41
zarina-kulm <zarina.kulmakhanbet@narxoz.kz>
2026-05-04 13:05:38 +0500

    add EnemyState interface

--- core/src/main/java/com/thrones/patterns/patterns/state/EnemyState.java
+++ core/src/main/java/com/thrones/patterns/patterns/state/EnemyState.java
@@ -1,7 +1,6 @@
 package com.thrones.patterns.patterns.state;
 
 import com.thrones.patterns.enemies.Enemy;
-
 public interface EnemyState {
     void update(Enemy enemy, float delta);
     String getStateName();
