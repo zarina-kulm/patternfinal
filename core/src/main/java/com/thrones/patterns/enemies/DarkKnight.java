5ace8a1bf23535cbb183df2d27ea6d7cf7b6700b
nazerkekalibek <nazerke.kalibek@narxoz.kz>
2026-05-05 23:12:43 +0500

    Added DarkKnight enemies

---
+++ core/src/main/java/com/thrones/patterns/enemies/DarkKnight.java
@@ -0,0 +1,42 @@
+package com.thrones.patterns.enemies;
+
+import com.thrones.patterns.characters.Character;
+
+public class DarkKnight extends Enemy{
+    private boolean blockActive=true;
+    private float blockCooldown=0f;
+    public DarkKnight(){
+        super("Dark Knight", 180f, 45f, 25f, 2f, 50, 80);
+    }
+
+    @Override
+    public void performAttack(Character target) {
+        System.out.println("Dark Knight strikes "+target.getName()+" for "+attack);
+        target.takeDamage(attack);
+    }
+
+    @Override
+    public void takeDamage(float damage){
+        if(blockActive){
+            System.out.println("Dark Knight BLOCKS!");
+            blockActive=false;
+            blockCooldown=5f;
+            return;
+        }
+        super.takeDamage(damage);
+    }
+
+    @Override
+    public void update(float delta){
+        super.update(delta);
+        if(blockCooldown>0){
+            blockCooldown-=delta;
+            if(blockCooldown<=0) blockActive=true;
+        }
+    }
+
+    @Override
+    public String getType() { 
+        return "DARK_KNIGHT"; 
+    }
+}
