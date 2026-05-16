ab5b4df3ee814f6f4fa2e86d71a3905d4e2cec57
markhabaerzhan <markhaba.erzhankyzy@narxoz.kz>
2026-05-16 14:21:09 +0500

    add WarOfRealms

--- core/src/main/java/com/thrones/patterns/WarOfRealms.java
+++ core/src/main/java/com/thrones/patterns/WarOfRealms.java
@@ -1,32 +1,23 @@
 package com.thrones.patterns;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.thrones.patterns.patterns.singleton.GameStateSingleton;
+import com.thrones.patterns.screens.MainMenuScreen;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class WarOfRealms extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class WarOfRealms extends Game {
+
+    public SpriteBatch batch;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        GameStateSingleton.getInstance().init();
+        setScreen(new MainMenuScreen(this));
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
     }
 }
